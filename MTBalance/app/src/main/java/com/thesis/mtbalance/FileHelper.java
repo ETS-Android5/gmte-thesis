fa7d229ba0e12425f48a0b4c9c29d927e8b233de
Mark Berentsen <markberentsen@hotmail.com>
2020-08-19 17:49:53 +0200

    Added array saving/loading methods to fileHelper

--- MTBalance/app/src/main/java/com/thesis/mtbalance/FileHelper.java
+++ MTBalance/app/src/main/java/com/thesis/mtbalance/FileHelper.java
@@ -1,5 +1,15 @@
 package com.thesis.mtbalance;
 
+import android.content.Context;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 /**
  * Helper class for file related methods.
  */
@@ -24,5 +34,58 @@ public class FileHelper {
             result[i] = Float.parseFloat(inputArray[i]);
 
         return result;
+    }
+
+    /**
+     * Saves an arrayList to the files directory, given the filename.
+     *
+     * @param fileName - the name of the new file.
+     * @param data     - the data to write to the file.
+     * @param context  - the application context.
+     */
+    public void saveArrayData(String fileName, ArrayList<String> data, Context context) {
+        try {
+            // Get the directory of the file
+            File fileDir = new File(context.getExternalFilesDir
+                    (null) + "/" + fileName + ".txt");
+
+            // Open new output streams and write the array to the file
+            FileOutputStream fos = new FileOutputStream(fileDir);
+            ObjectOutputStream oot = new ObjectOutputStream(fos);
+            oot.writeObject(data);
+            oot.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Loads an arrayList from the files directory, given the filename.
+     *
+     * @param fileName - the name of the file to load.
+     * @param context  - the application context.
+     * @return an arrayList converted from the files directory.
+     */
+    public ArrayList<String> loadArrayData(String fileName, Context context) {
+        // Initialize a new arrayList
+        ArrayList<String> data = null;
+
+        try {
+            // Get the directory of the file
+            File fileDir = new File(context.getExternalFilesDir
+                    (null) + "/" + fileName + ".txt");
+
+            // Open new input streams and write the file to the array
+            FileInputStream fis = new FileInputStream(fileDir);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            data = (ArrayList<String>) ois.readObject();
+            ois.close();
+            fis.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return data;
     }
 }
