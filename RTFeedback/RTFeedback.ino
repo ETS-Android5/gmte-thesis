42621d480dfde5951646c8fab32c65ff6d672f74
Mark Berentsen <markberentsen@hotmail.com>
2020-09-23 15:14:03 +0200

    Fixed bug expecting int instead of char for switches

--- RTFeedback/RTFeedback.ino
+++ RTFeedback/RTFeedback.ino
@@ -20,7 +20,6 @@ char BLEData[2];
 int P = 0;
 
 void setup() {
-  Serial.begin(9600); // Remove when sketch is finished
   BLE.begin(9600);
   Leds.begin();
   for (int i = FPIN; i <= LPIN; i++)
@@ -42,13 +41,13 @@ void loop() {
 
 void selectFeedback() {
   switch (BLEData[0]) {
-    case 1:
+    case '1':
       feedbackVT();
       break;
-    case 2:
+    case '2':
       feedbackVD();
       break;
-    case 3:
+    case '3':
       feedbackVP();
       break;
   }
@@ -62,28 +61,28 @@ void feedbackVT() {
     digitalWrite(i, LOW);
 
   switch (BLEData[1]) {
-    case 0:
+    case '0':
       digitalWrite(FPIN, HIGH);
       break;
-    case 1:
+    case '1':
       digitalWrite(FPIN + 1, HIGH);
       break;
-    case 2:
+    case '2':
       digitalWrite(FPIN + 2, HIGH);
       break;
-    case 3:
+    case '3':
       digitalWrite(FPIN + 3, HIGH);
       break;
-    case 4:
+    case '4':
       digitalWrite(FPIN + 4, HIGH);
       break;
-    case 5:
+    case '5':
       digitalWrite(FPIN + 5, HIGH);
       break;
-    case 6:
+    case '6':
       digitalWrite(FPIN + 6, HIGH);
       break;
-    case 7:
+    case '7':
       digitalWrite(FPIN + 7, HIGH);
       break;
   }
@@ -96,28 +95,28 @@ void feedbackVD() {
   Leds.clear();
 
   switch (BLEData[1]) {
-    case 0:
+    case '0':
       Leds.fill(Green, 6, 4);
       break;
-    case 1:
+    case '1':
       Leds.fill(Green, 9, 4);
       break;
-    case 2:
+    case '2':
       Leds.fill(Green, 12, 4);
       break;
-    case 3:
+    case '3':
       Leds.fill(Red, 9, 4);
       break;
-    case 4:
+    case '4':
       Leds.fill(Red, 6, 4);
       break;
-    case 5:
+    case '5':
       Leds.fill(Red, 3, 4);
       break;
-    case 6:
+    case '6':
       Leds.fill(Green, 0, 4);
       break;
-    case 7:
+    case '7':
       Leds.fill(Green, 3, 4);
       break;
   }
@@ -132,31 +131,31 @@ void feedbackVP() {
   Leds.clear();
 
   switch (BLEData[1]) {
-    case 0:
+    case '0':
       Leds.fill(Blue, 6, 4);
       break;
-    case 1:
+    case '1':
       Leds.fill(Blue, 9, 4);
       break;
-    case 2:
+    case '2':
       Leds.fill(Blue, 12, 4);
       break;
-    case 3:
+    case '3':
       Leds.setPixelColor(0, Blue);
       Leds.fill(Blue, 13, 3);
       break;
-    case 4:
+    case '4':
       Leds.fill(Blue, 0, 2);
       Leds.fill(Blue, 14, 2);
       break;
-    case 5:
+    case '5':
       Leds.fill(Blue, 0, 3);
       Leds.setPixelColor(15, Blue);
       break;
-    case 6:
+    case '6':
       Leds.fill(Blue, 0, 4);
       break;
-    case 7:
+    case '7':
       Leds.fill(Blue, 3, 4);
       break;
   }
