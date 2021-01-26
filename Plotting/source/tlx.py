b56cd16ed5c3c2b7939570bf7daf095a936c006e
Mark Berentsen <markberentsen@hotmail.com>
2021-01-26 17:06:54 +0100

    Updated TLX plot to show the full TLX as well

--- Plotting/source/tlx.py
+++ Plotting/source/tlx.py
@@ -38,15 +38,15 @@ for condition in data:
     allstdevs.append(statistics.stdev(condition))
 
 # split the mean and stdev arrays
-submeans = np.array_split(allmeans, 6)
-substdevs = np.array_split(allstdevs, 6)
+submeans = np.array_split(allmeans, 7)
+substdevs = np.array_split(allstdevs, 7)
 
 # sort the arrays to be grouped per condition
 means = [list(x) for x in zip(*submeans)]
 stdevs = [list(x) for x in zip(*substdevs)]
 
 # make labels, a color list and legend for each of the conditions
-labels = ["Mental Demand", "Physical Demand", "Temporal Demand", "Performance", "Effort", "Frustration"]
+labels = ["TLX", "Mental Demand", "Physical Demand", "Temporal Demand", "Performance", "Effort", "Frustration"]
 condcolors = ["tab:brown", "tab:purple", "tab:green", "tab:cyan"] # tab:gray for VCF
 legend = ["APP", "VTF", "VDF", "VPF"]
 
@@ -60,16 +60,18 @@ fig, ax = plt.subplots(figsize=(15, 5))
 
 # annotate barplot with significance lines 
 maxmeans = []
-for i in range(6):
+for i in range(7):
     maxmeans.append(max(submeans[i]) + substdevs[i][np.argmax(submeans[i])])
 
-annotate_significance(ax, barpos[2][0], barpos[3][0], maxmeans[0] + (maxmeans[0] * 0.05), "**") 
-annotate_significance(ax, barpos[1][0], barpos[3][0], maxmeans[0] + (maxmeans[0] * 0.15), "**") 
-annotate_significance(ax, barpos[0][0], barpos[3][0], maxmeans[0] + (maxmeans[0] * 0.25), "**") 
-annotate_significance(ax, barpos[1][3], barpos[3][3], maxmeans[3] + (maxmeans[3] * 0.05), "**") 
-annotate_significance(ax, barpos[1][5], barpos[2][5], maxmeans[5] + (maxmeans[5] * 0.05), "*") 
-annotate_significance(ax, barpos[1][5], barpos[3][5], maxmeans[5] + (maxmeans[5] * 0.15), "**") 
-annotate_significance(ax, barpos[0][5], barpos[3][5], maxmeans[5] + (maxmeans[5] * 0.25), "**") 
+annotate_significance(ax, barpos[1][0], barpos[3][0], maxmeans[0] + (maxmeans[0] * 0.05), "**")
+annotate_significance(ax, barpos[0][0], barpos[3][0], maxmeans[0] + (maxmeans[0] * 0.15), "*")
+annotate_significance(ax, barpos[2][1], barpos[3][1], maxmeans[1] + (maxmeans[1] * 0.05), "**") 
+annotate_significance(ax, barpos[1][1], barpos[3][1], maxmeans[1] + (maxmeans[1] * 0.15), "**") 
+annotate_significance(ax, barpos[0][1], barpos[3][1], maxmeans[1] + (maxmeans[1] * 0.25), "**") 
+annotate_significance(ax, barpos[1][4], barpos[3][4], maxmeans[4] + (maxmeans[4] * 0.05), "**") 
+annotate_significance(ax, barpos[1][6], barpos[2][6], maxmeans[6] + (maxmeans[6] * 0.05), "*") 
+annotate_significance(ax, barpos[1][6], barpos[3][6], maxmeans[6] + (maxmeans[6] * 0.15), "**") 
+annotate_significance(ax, barpos[0][6], barpos[3][6], maxmeans[6] + (maxmeans[6] * 0.25), "**") 
  
 # generate barplots
 for i in range(4):
@@ -85,9 +87,9 @@ ax.set_ylabel("NASA TLX Score", weight="bold")
 
 ax.set_xticks(x)
 ax.set_xticklabels(labels)
-ax.set_xlabel("NASA TLX Sub-Scales", weight="bold")
+ax.set_xlabel("NASA TLX Scales", weight="bold")
 
-ax.legend()
+ax.legend(loc=9)
 
 # save and show the plot!
 name = "TLX.png"
